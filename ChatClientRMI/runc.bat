4860b8fa17eb40761dfe2c04776b3397831ea156
Franklin Cheung <Sonofmath@gmail.com>
2010-12-05 15:22:25 -0500

    Removing batch files

--- ChatClientRMI/runc.bat
+++
@@ -1,1 +0,0 @@
-java -Djava.security.policy=..\policy.jc -Djava.naming.factory.initial=com.sun.jndi.cosnaming.CNCtxFactory -Djava.naming.provider.url=iiop://localhost:900 ChatClientRMI.Client %1
--- env.bat
+++
@@ -1,3 +0,0 @@
-set CLASSPATH=c:\rmi-iiop;C:\Program Files\Java\jdk1.6.0_22\bin;c:\RMI-IIOP-MOO
-
-
